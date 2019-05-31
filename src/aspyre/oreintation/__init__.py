ea320ef7791e0b0ab1550d9add1417ebd6da7071
Junchao Xia <junchao.xia@princeton.edu>
2019-05-31 18:07:56 -0400

    add the class design for estimating 3D orientations using common lines.

---
+++ src/aspyre/oreintation/__init__.py
@@ -0,0 +1,40 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class Oreint3D:
+
+    def __init__(self, src):
+        """
+        constructor of an object for estimating 3D oreintations
+        """
+        pass
+
+    def get_commonlines(self):
+        """
+        find the commonlines by voting method
+        """
+        pass
+
+    def build_clmatrix(self):
+        """
+        build the commonline matrix
+        """
+        pass
+
+    def estimate(self):
+        """
+        perform classifying 2D images
+        """
+        pass
+
+    def output(self):
+        """
+        Output the clean images
+        """
+        pass
+
+
+
+
---
+++ src/aspyre/oreintation/commonline_lud.py
@@ -0,0 +1,34 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class CommLineLUD(Oreint3D):
+    """
+    Define a class to estimate 3D oreintations using Least Unsquared Deviations described as below:
+    L. Wang, A. Singer, and  Z. Wen, Orientation Determination of Cryo-EM Images Using Least Unsquared Deviations,
+    SIAM J. Imaging Sciences, 6, 2450-2483 (2013).
+
+    """
+
+    def __init__(self, src):
+        """
+        constructor of an object for estimating 3D oreintations
+        """
+        pass
+
+    def estimate(self):
+        """
+        perform classifying 2D images
+        """
+        pass
+
+    def output(self):
+        """
+        Output the clean images
+        """
+        pass
+
+
+
+
