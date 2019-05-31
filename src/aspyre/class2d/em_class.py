52b460da8a02580306f8ae4f056273b3c655a4cf
Junchao Xia <junchao.xia@princeton.edu>
2019-05-31 17:22:29 -0400

    add the class design for 2D classification

---
+++ src/aspyre/class2d/em_class.py
@@ -0,0 +1,29 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class EM_Class2D(Class2D):
+
+    def __init__(self, src):
+        """
+        constructor of an object for classifying 2D images using
+        Expectation-Maximization (EM) algorithm
+        """
+        pass
+
+    def classify(self, src):
+        """
+        perform classifying 2D images
+        """
+        pass
+
+    def output(self):
+        """
+        Output the clean images
+        """
+        pass
+
+
+
+
