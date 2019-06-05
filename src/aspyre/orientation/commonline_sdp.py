f4b3c5e51db827b920e969c131de286350d04b0a
Junchao Xia <junchao.xia@princeton.edu>
2019-06-05 15:02:15 -0400

    fix typoes and misleading descriptions.

---
+++ src/aspyre/orientation/commonline_sdp.py
@@ -0,0 +1,35 @@
+from aspyre.orientation import Orient3D
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class CommLineSDP(Orient3D):
+    """
+    Define a derived class to estimate 3D orientations using Semi-Definite Programing described as below:
+    A. Singer and Y. Shkolnisky, Three-Dimensional Structure Determination from Common Lines in Cryo-EM by
+    Eigenvectors and Semidefinite Programming, SIAM J. Imaging Sciences, 4, 543-572 (2011).
+
+    """
+
+    def __init__(self, src):
+        """
+        constructor of an object for estimating 3D orientations
+        """
+        pass
+
+    def estimate(self):
+        """
+        perform estimation of orientations
+        """
+        pass
+
+    def output(self):
+        """
+        Output the 3D orientations
+        """
+        pass
+
+
+
+
